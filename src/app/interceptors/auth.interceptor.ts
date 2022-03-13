9476f78035083f7ba1952f0247975f9e0ce03263
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-03-13 08:13:46 -0400

    creacion de middleware para enviar el jwt

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http";
+import { AuthService } from "../services/auth.service";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const authToken = this.authService.getToken();
+    console.log(authToken);
+    req = req.clone({
+      setHeaders: {
+        Authorization: "Bearer " + authToken
+      }
+    });
+    return next.handle(req);
+  }
+
+}
